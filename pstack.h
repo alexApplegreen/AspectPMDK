4c34a584edf83ec6199dac743c5e5a3b4e6869fa
Alexander Tepe <alexander.tepe@hotmail.de>
2020-08-06 14:59:51 +0200

    WIP

--- pstack.h
+++ pstack.h
@@ -14,90 +14,44 @@
 #include <libpmemobj.h>
 
 #define POOL_PATH "/mempool"
-#define TYPE "pstack"
-#define TYPE_NUM 1
+
+static PMEMobjpool* pool;
+
+struct pstack {
+    PMEMoid                 elements;
+    uint64_t                maxsize;
+    uint64_t                counter;
+};
+
+int init(PMEMobjpool* pool, void* ptr, void* args) {
+    int size = (int)args;
+    struct pstack* stack = (struct pstack*)ptr;
+    struct pobj_action act[2];
+
+    stack->maxsize = size;
+    stack->counter = 0;
+    stack->elements = pmemobj_reserve(pool, act + 0, size, 0);
+
+    return 0;
+}
 
 // global TOID
 TOID_DECLARE(struct pstack, 1);
 TOID(struct pstack) pstack_oid;
 
-struct pstack {
-    char*                   elements;
-    char                    top;
-    uint64_t                maxsize;
-    uint64_t                counter;
-
-    // Stack stores reference to it's own memory pool
-    PMEMobjpool*            pool;
-};
-
 /// Constructor
-struct pstack* getInstance(int size) {
-
-    // initialize Stack
-    struct pstack* stack = malloc(sizeof(struct pstack));
-
-    D_RW(pstack_oid)->maxsize = size;
-    D_RW(pstack_oid)->elements = malloc(size * sizeof(char));
-    D_RW(pstack_oid)->counter = 0;
+PMEMoid getInstance(int size) {
 
     // allocate persistent memory
-    if (!(stack->pool = pmemobj_create(POOL_PATH, "", size * sizeof(char), 0666))) {
+    if (!(pool = pmemobj_create(POOL_PATH, " ", size * sizeof(char), 0666))) {
         perror("pmemobj_create");
         exit(1);
     }
 
-    return stack;
-}
+    // initialize Stack
+    pmemobj_alloc(pool, pstack_oid, sizeof(struct pstack), 1, init, (void*)size);
 
-/// adds element to the stack
-void push(struct pstack* stack, char elem) {
-    if (stack->counter >= stack->maxsize) {
-        printf("%s\n", "Stack is full!");
-    }
-    else {
-        TX_BEGIN(D_RW(pstack_oid)->pool) {
-            TX_ADD_DIRECT(D_RW(pstack_oid)->elements);
-            TX_ADD_DIRECT(&D_RW(pstack_oid)->counter);
-
-            stack->elements[++stack->counter] = elem;
-        } TX_END
-
-    }
-}
-
-/// gets element from stack and deletes it according to FIFO principle
-char pop(struct pstack* stack) {
-    if (stack->counter <= 0) {
-        printf("%s\n", "Stack is empty");
-    }
-    else {
-        TX_BEGIN(stack->pool) {
-            TX_ADD_DIRECT(D_RW(pstack_oid)->elements);
-            TX_ADD_DIRECT(&D_RW(pstack_oid)->counter);
-
-            char elem = stack->elements[stack->counter];
-
-            stack->counter--;
-            return elem;
-        } TX_END
-
-    }
-    return NULL;
-}
-
-/// checks if given Stack is empty
-int isEmpty(struct pstack* stack) {
-    return stack->counter == 0;
-}
-
-/*!
-   \brief Since structs do not define own destructors, it is in the Responsibility
-          of the application developer to free persistent memory after the stack
-          is out of scope.
-
-   \param Pointer to the pstack
-*/
-void free_pool(struct pstack* stack) {
-    pmemobj_close(stack->pool);
+    PMEMoid pstack;
+    TOID_ASSIGN(pstack_oid, pstack);
+    return pstack;
 }
--- pstacktest.c
+++ pstacktest.c
@@ -1,50 +1,11 @@
 #include "pstack.h"
 #include <stdio.h>
+#include <libpmemobj.h>
 
 int passed = 0;
 
 int main(int argc, char const *argv[]) {
-    struct pstack* pstack = getInstance(10);
-
-    if (pstack == NULL) {
-        printf("%s\n", "Stack is Null!");
-        passed--;
-    }
-    else {
-        // Elemente in den Stack packen
-        for (int i = 0; i < 11; i++) {
-            push(pstack, 'A');
-        }
-        if (pstack->counter == 11) {
-            printf("%s\n", "Can push too many elements!");
-            passed--;
-        }
-
-        // clear too many elements
-        for (int i = 0; i < 11; i++) {
-            pop(pstack);
-        }
-        if (pstack->counter != 0) {
-            printf("%s\n", "Can clear too many elements");
-            passed--;
-        }
-
-        // push two and check for them
-        push(pstack, 'B');
-        push(pstack, 'A');
-
-        if (pop(pstack) != 'A') {
-            passed--;
-        }
-        if (pop(pstack) != 'B') {
-            passed--;
-        }
-        printf("%s\n", "FIFO does not work!");
-    }
-
-    if (passed == 0) {
-        printf("%s\n", "ALL PASSED");
-    }
+    PMEMoid pstack = getInstance(10);
 
     return 0;
 }
