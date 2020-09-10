8b5a002448671f9a254f215cdb6729571b65d3d8
Alexander Tepe <alexander.tepe@hotmail.de>
2020-09-10 13:51:05 +0200

    Beispiel für Präsentation

---
+++ bsp/HelloWorld.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+int main(int argc, char const *argv[]) {
+    Hello::say_hello();
+    return 0;
+}
---
+++ bsp/hello.h
@@ -0,0 +1,14 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <stdio.h>
+
+class Hello {
+
+public:
+    static void say_hello() {
+        printf("%s\n", " Welt ");
+    }
+};
+
+#endif
