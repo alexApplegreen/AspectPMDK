aa537d7e1f952bab0715c6d480dc9a2c2473db01
Alexander Tepe <alexander.tepe@hotmail.de>
2020-10-14 16:46:22 +0200

    Added Catch and unit tests for Stack class

---
+++ src/stacktest.cpp
@@ -0,0 +1,24 @@
+#include "util/catch.hpp"
+#include "stack.h"
+
+#define CATCH_CONFIG_MAIN
+
+Stack stack(10);
+
+TEST_CASE("Get Instance", "[Stack]") {
+    REQUIRE(stack.isEmpty());
+}
+
+TEST_CASE("Can push", "[Stack]") {
+    stack.push('O');
+    REQUIRE(!stack.isEmpty());
+}
+
+TEST_CASE("Can pop", "[Stack]") {
+    char elem = stack.pop();
+    REQUIRE(elem == 'O');
+}
+
+TEST_CASE("Is empty again", "[Stack]") {
+    REQUIRE(stack.isEmpty());
+}
