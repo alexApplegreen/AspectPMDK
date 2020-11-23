2ed43bd7f6ddf2be31536eb76f0c8beeed079120
altepe <altepe@paros.inf.uni-osnabrueck.de>
2020-11-23 13:06:08 +0100

    kommentare

--- Namespace_Bug/src/class.h
+++ Namespace_Bug/src/class.h
@@ -8,8 +8,11 @@ class Testclass {
 
 public:
 
+    // Hier soll nur pointcut1 in aspect1.ah angewebt werden.
     [[namespace1::attr]]
     static void printAnnotation() {
+
+        // Ausgabe soll also lauten: "Should say namespace1: namespace1"
         std::cout << "Should say namespace1: ";
     }
 };
