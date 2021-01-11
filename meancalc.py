043825bf959da370aa01c3fd2583122321c20b84
AlexApplegreen <alexander.tepe@hotmail.de>
2021-01-11 14:11:52 +0100

    meancalc.py berechnet jetzt min und max

--- meancalc.py
+++ meancalc.py
@@ -1,10 +1,19 @@
 import sys
 
-file = open(sys.arg[1], 'r')
+file = open(sys.argv[1], 'r')
 lines = file.readlines()
 
 sum = 0
+min = sys.maxint
+max = 0
 for line in lines:
-    sumn += line
+    sum += int(line)
+    if int(line) < min :
+        min = int(line)
 
-print(sum /= 100)
+    if int(line) > max :
+        max = int(line)
+
+print("mean: {}".format(sum / 100))
+print("min: {}".format(min))
+print("max: {}".format(max))
